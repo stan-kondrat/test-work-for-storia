cbb1a9537a88bfa191ff5d4b1c01996d6ed55e9a
Stan Kondrat <stan.kondrat@gmail.com>
2015-11-01 19:48:27 +0300

    auth and loaf content

--- src/api/feed.js
+++ src/api/feed.js
@@ -4,8 +4,50 @@ import { Router } from 'express';
 import jade from 'jade';
 import fm from 'front-matter';
 import fs from '../utils/fs';
+import request from 'superagent';
+import Qs from 'qs';
 
 const router = new Router();
+
+const auth = () => {
+  return new Promise((resolve, reject) => {
+    const authUrl = "https://storia.me/api/acl/auth/Selfish/";
+    const username = "test_task@example.com";
+    const password = "qwe123";
+    request.post(authUrl + username)
+      .set('Content-Type', 'application/json')
+      .send('{"password":"' + password + '", "remember":false, "token":""}')
+      .end(function (err, res) {
+        if (res.ok) {
+          try {
+            const authResp = JSON.parse(res.text);
+            resolve(authResp.sessionId);
+          } catch (e) {
+            reject();
+          }
+        } else {
+          reject();
+        }
+      });
+  });
+};
+
+const loadContent = () => {
+  return new Promise((resolve, reject) => {
+    auth().then((sessionId) => {
+      const contentUrl = "https://storia.me/api/feed/content";
+      request.get(contentUrl)
+        .set('Cookie', 'SSID='+sessionId)
+        .end(function (err, res) {
+          if (res.ok) {
+            resolve(res.body.items || []);
+          } else {
+            //todo
+          }
+        });
+    })
+  });
+}
 
 router.get('/', async (req, res, next) => {
   try {
@@ -16,8 +58,18 @@ router.get('/', async (req, res, next) => {
       title: '',
       component: 'FeedPage'
     };
+    const query = Qs.parse(req.query) || {};
 
-    res.status(200).send(content);
+    if (typeof(query.action) == 'undefined'){
+      loadContent().then((items)=>{
+        content.items = items;
+        res.status(200).send(content);
+      });
+
+    } else if(query.action == "todo"){
+      //todo
+    }
+
   } catch (err) {
     next(err);
   }
--- src/components/FeedPage/FeedPage.js
+++ src/components/FeedPage/FeedPage.js
@@ -12,13 +12,27 @@ class FeedPage extends Component {
     onPageNotFound: PropTypes.func.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {items: props.items || []};
+  }
+
   render() {
     const title = 'Storia demo client';
     this.context.onSetTitle(title);
+    var postNodes = this.state.items.map(function (post) {
+      return (
+        <div>
+          {post.objectPreview.id} - {post.objectPreview.title}
+        </div>
+      );
+    });
+
     return (
       <div className="FeedPage">
         <div className="FeedPage-container">
-          {title}
+          <h1>{title}</h1>
+          {postNodes}
         </div>
       </div>
     );
